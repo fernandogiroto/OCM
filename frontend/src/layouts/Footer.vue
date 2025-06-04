9922b1738cb86ac7e8120e7e11c5831c2f7caaf7
fernandogiroto <giroto.luis@gmail.com>
2025-06-04 10:05:18 +0200

    [frontend] - Styles, SASS & Architecture Added

---
+++ frontend/src/layouts/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        footer
+    </footer>
+</template>
+    
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+  @use '@/scss/mixings';
+  @use '@/scss/variables';
+
+  footer {
+    @include mixings.flexbox(column, center, center);
+    width: 100%;
+    height: 50px;
+    padding: 20px;
+    border-top:  1px solid var(--text-color);
+  }
+
+</style>
---
+++ frontend/src/layouts/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+      <span>Logo</span>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+  @use '@/scss/mixings';
+  @use '@/scss/variables';
+
+  header {
+    @include mixings.flexbox(row, space-between, center);
+    width: 100%;
+    height: 50px;
+    padding: 20px;
+    border-bottom: 1px solid var(--text-color);
+  }
+
+</style>
